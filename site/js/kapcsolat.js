930b68bcd50fa8debb7f6e6957c8e8ca352d1f49
Tamas Hegedus <sorgloomer@gmail.com>
2015-08-03 22:21:36 +0200

    working on new texts, and some styling, filled contact menu with info, hided map, changed navbar items

--- site/js/kapcsolat.js
+++ site/js/kapcsolat.js
@@ -1,21 +1,23 @@
 var googleMap;
 
 function initialize() {
-  var center = new google.maps.LatLng(48.1031047,20.7916887);
-  var mapProp = {
-    center: center,
-    zoom: 17,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+  var map_element = document.getElementById("google-map");
+  if (map_element) {
+    var center = new google.maps.LatLng(48.1031047,20.7916887);
+    var mapProp = {
+      center: center,
+      zoom: 17,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-  googleMap = new google.maps.Map(document.getElementById("google-map"), mapProp);
+    googleMap = new google.maps.Map(map_element, mapProp);
+    new google.maps.Marker({
+      position: center,
+      map: googleMap,
+      title: 'Tökéletes Összhang szalon'
+    });
 
-  new google.maps.Marker({
-    position: center,
-    map: googleMap ,
-    title: 'Tökéletes Összhang szalon'
-  });
-
+  }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
